6366b61a79957352be10e5061c8317d7578958f6
Farzad Bekran <farzadbekran@gmail.com>
2023-09-27 11:04:01 +0330

    added Surreal monad

--- src/Database/Surreal/WS/RPC/Surreal.hs
+++ src/Database/Surreal/WS/RPC/Surreal.hs
@@ -2,49 +2,34 @@
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE FlexibleContexts #-}
 
-module Database.Surreal.WS.RPC.Surreal
-    ( ConnectionInfo (..)
-    , QueryResult (..)
-    , Response (..)
-    , Error (..)
-    , app
-    , connect
-    , defaultConnectionInfo
-    , send
-    ) where
+module Database.Surreal.WS.RPC.Surreal where
 
 import           ClassyPrelude        hiding ( error, id )
-import           Control.Concurrent   ( forkIO, threadDelay )
+import           Control.Concurrent   ( ThreadId, forkIO, threadDelay )
 import           Control.Exception    ( throw )
 import           Data.Aeson           ( FromJSON, ToJSON, Value, decode,
                                         encode )
 import qualified Data.ByteString.Lazy as BL
 import           Data.Map.Strict      as M
-import           GHC.IO               ( unsafePerformIO )
 import           Network.Socket       ( withSocketsDo )
 import qualified Network.WebSockets   as WS
+import Control.Monad.Except (MonadError)
 
-conn :: TVar (Either SomeException WS.Connection)
-{-# NOINLINE conn #-}
-conn = unsafePerformIO $ newTVarIO $ Left $ SomeException NotConnected
+type Surreal a = ReaderT ConnectionState IO a
 
-nextReqID :: TVar Int
-{-# NOINLINE nextReqID #-}
-nextReqID = unsafePerformIO $ newTVarIO 0
-
--- A map of request id -> MVar Response.
--- Used to return the server response to the caller
--- `app` thread parses the server messages and writes them
--- to this map using the id included in the messages
-respMap :: TVar (Map Int (MVar Response))
-{-# NOINLINE respMap #-}
-respMap = unsafePerformIO $ newTVarIO M.empty
+data ConnectionState
+  = ConnectionState
+      { conn           :: WS.Connection
+      , nextReqID      :: TVar Int
+      , respMap        :: TVar (Map Int (MVar Response))
+      , listenerThread :: ThreadId
+      }
 
 data NetworkException
   = NotConnected
   | InvalidResponse Text
-  | MissingMVar Int
   | RequestTimeout Request
   deriving (Exception, Show)
 
@@ -52,109 +37,102 @@ data Request
   = Request
       { id     :: Int
       , method :: Text
-      , params :: [Text]
+      , params :: [Value]
       }
   deriving (Eq, FromJSON, Generic, Read, Show, ToJSON)
 
 data Response
   = Response
       { id     :: Int
-      , result :: Maybe [QueryResult]
+      , result :: Maybe Value
       , error  :: Maybe Error
       }
   deriving (Eq, FromJSON, Generic, Read, Show, ToJSON)
 
-data QueryResult
-  = QueryResult
-      { result :: Maybe Value
-      , status :: Text
-      , time   :: Text
-      }
-  deriving (Eq, FromJSON, Generic, Read, Show, ToJSON)
+-- data QueryResult
+--   = QueryResult
+--       { result :: Maybe Value
+--       , status :: Text
+--       , time   :: Text
+--       }
+--   deriving (Eq, FromJSON, Generic, Read, Show, ToJSON)
 
 data Error
   = Error
       { code    :: Int
       , message :: Text
       }
-  deriving (Eq, Generic, FromJSON, Read, Show, ToJSON)
+  deriving (Eq, FromJSON, Generic, Read, Show, ToJSON)
 
 data ConnectionInfo
   = ConnectionInfo
       { url  :: String
       , port :: Int
+      , user :: Text
+      , pass :: Text
+      , ns   :: Text
+      , db   :: Text
       }
   deriving (Eq, Read, Show)
 
 defaultConnectionInfo :: ConnectionInfo
-defaultConnectionInfo = ConnectionInfo "0.0.0.0" 8000
+defaultConnectionInfo = ConnectionInfo "0.0.0.0" 8000 "root" "root" "test" "test"
 
-app :: MVar SomeException -> WS.ClientApp ()
-app exceptionMVar connection = do
-  atomically $ writeTVar conn $ Right connection
-  -- if anything throws, we write the exception into the exceptionMVar so that
-  -- the thread started in the `connect` function can try to reconnect
-  catchAny
-    (forever $ do
-        msg <- WS.receiveData connection
-        let rsp = decode msg :: Maybe Response
-        case rsp of
-          Just r@Response { .. } -> do
-            mvarMap <- readTVarIO respMap
-            case mvarMap !? id of
-              Just mvar -> putMVar mvar r
-              Nothing   -> throw $ MissingMVar id
-          Nothing -> throw $ InvalidResponse $ decodeUtf8 $ BL.toStrict msg)
-    (\e -> do
-        _ <- tryAny $ WS.sendClose connection ("Bye!" :: Text)
-        atomically $ writeTVar conn $ Left e
-        putMVar exceptionMVar e)
+{- |
+starts a thread to listen to the connection and write responses to the
+response map. return the threds ID
+-}
+app :: MVar WS.Connection -> TVar (Map Int (MVar Response)) -> WS.ClientApp ThreadId
+app connMvar respMap connection = do
+  putMVar connMvar connection
+  forkIO $ forever $ do
+    msg <- WS.receiveData connection
+    let rsp = decode msg :: Maybe Response
+    case rsp of
+      Just r@Response { .. } -> do
+        mvarMap <- readTVarIO respMap
+        case mvarMap !? id of
+          Just mvar -> putMVar mvar r
+          Nothing   -> putStrLn "app: Missing MVar!"
+      Nothing -> putStrLn $ "app: Invalid Response: " <> decodeUtf8 (BL.toStrict msg)
 
-connect :: MonadIO m => ConnectionInfo -> m ()
-connect ci@ConnectionInfo { .. } = do
-  mCurrentConn <- readTVarIO conn
-  -- we use this to reconnect to db in case client app throws
-  exceptionMVar <- newEmptyMVar
-  let doConnect = withSocketsDo $ catchAny
-                   (WS.runClient url port "/rpc" (app exceptionMVar))
-                   (putMVar exceptionMVar)
-  _ <- liftIO $ case mCurrentConn of
-    Left _ -> forkIO doConnect
-    Right connection -> do
-      -- try to close the connection if possible
-      _ <- tryAny $ WS.sendClose connection ("Bye!" :: Text)
-      forkIO $ withSocketsDo doConnect
-  _ <- liftIO $ forkIO $ do
-    res <- readMVar exceptionMVar
-    atomically $ writeTVar conn $ Left res
-    threadDelay $ 10 * 1000000
-    connect ci
-  return ()
+connect :: MonadIO m => ConnectionInfo -> m ConnectionState
+connect ConnectionInfo { .. } = do
+  connMVar <- newEmptyMVar
+  respTVar <- newTVarIO M.empty
+  tid <- liftIO $ withSocketsDo $ WS.runClient url port "/rpc" (app connMVar respTVar)
+  conn <- readMVar connMVar
+  reqIDTvar <- newTVarIO 0
+  let connectionState = ConnectionState conn reqIDTvar respTVar tid
+  -- TODO: signin before returning
+  return connectionState
 
-getNextRequestID :: MonadIO m => m Int
-getNextRequestID = atomically $ do
-  nextID <- readTVar nextReqID
-  writeTVar nextReqID $ nextID + 1
-  return nextID
+getNextRequestID :: Surreal Int
+getNextRequestID = do
+  ConnectionState { .. } <- ask
+  atomically $ do
+    nextID <- readTVar nextReqID
+    writeTVar nextReqID $ nextID + 1
+    return nextID
 
-send :: ( MonadUnliftIO m) => Text -> [Text] -> m Response
+send :: Text -> [Value] -> Surreal Response
 send method params = do
-  mCurrentConn <- readTVarIO conn
-  case mCurrentConn of
-    Left e -> throw e
-    Right connection -> do
-      nextID <- getNextRequestID
-      mvar <- newEmptyMVar
-      atomically $ modifyTVar' respMap (M.insert nextID mvar)
-      let req = Request nextID method params
-      liftIO $ WS.sendTextData connection $ encode req
-      -- recover after 10 seconds of delay
-      res <- race
-        (do
-          liftIO $ threadDelay $ 10 * 1000000
-          return $ RequestTimeout req)
-        (readMVar mvar)
-      atomically $ modifyTVar' respMap (M.delete nextID)
-      case res of
-        Right r -> return r
-        Left e  -> throw e
+  ConnectionState { .. } <- ask
+  nextID <- getNextRequestID
+  mvar <- newEmptyMVar
+  atomically $ modifyTVar' respMap (M.insert nextID mvar)
+  let req = Request nextID method params
+  liftIO $ WS.sendTextData conn $ encode req
+  -- recover after 10 seconds of delay
+  res <- race
+    (do
+      liftIO $ threadDelay $ 10 * 1000000
+      return $ RequestTimeout req)
+    (readMVar mvar)
+  atomically $ modifyTVar' respMap (M.delete nextID)
+  case res of
+    Right r -> return r
+    Left e  -> throw e
+
+runSurreal :: MonadIO m => ConnectionState -> Surreal a -> m a
+runSurreal cs surr = liftIO $ runReaderT surr cs
